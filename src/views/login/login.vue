b559c3646216e36b890f6e5ea044ef99f7f58888
justic <1131918725@qq.com>
2024-10-20 16:31:07 +0800

    feat:完成登录表单页面样式

---
+++ src/views/login/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div
+    class="container d-flex justify-content-center align-items-center vh-100"
+  >
+    <div class="col-4">
+      <form class="shadow-sm rounded p-4 border">
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">用户名</label>
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">密码</label>
+          <input
+            type="password"
+            class="form-control"
+            id="exampleInputPassword1"
+          />
+        </div>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+          <label class="form-check-label" for="exampleCheck1">记住我</label>
+        </div>
+        <div class="d-flex flex-column gap-2 align-items-center px-1">
+          <button type="submit" class="btn btn-primary w-100 mb-2 text-white">
+            登录
+          </button>
+          <router-link
+            to="/reg"
+            class="w-100 border rounded text-decoration-none text-center"
+          >
+            <button type="submit" class="btn">注册</button>
+          </router-link>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import axios from "@/utils/axios";
+</script>
+
+<style></style>
